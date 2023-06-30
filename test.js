7233f3da711fcd801ed698df8af38559a935fefc
RushiNCode93 <ncode.rushikesh@gmail.com>
2023-06-30 15:00:20 +0530

    Updated files

--- test.js
+++
@@ -1,6 +0,0 @@
-window.alert("Custom Script Tag.");
-console.log("Coming here...");
-alert("coming here...");
-jQuery("form[action='/cart/add']").each(function() {
-  console.log("I'm in product page");
-});
